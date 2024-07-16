9954596b498e35e762db4e1e673d58f4fb8103aa
Yash Kumar <dev.yash2k03@gmail.com>
2024-07-16 19:35:24 +0530

    Add folder structure for daily tasks

---
+++ Day_01_Challenge/Activity_1.js
@@ -0,0 +1,15 @@
+// Activity 1: Variable Declaration
+
+// TASk 1
+// Declare a vriable using 'var', assign it a number, and log the value to the console.
+
+var num = 1234
+console.log(typeof num);
+console.log(num);
+
+// TASK 2
+// Declare a variable using 'let', assign it a string, and log the value to the console.
+
+let myName = "Yash"
+console.log(typeof myName);
+console.log(myName);
---
+++ Day_01_Challenge/Activity_2.js
@@ -0,0 +1,8 @@
+// Activity 2: Constant Declaration
+
+// TASk 3
+// Declare a variable using 'const', assign it a boolean value, and log the value to the console.
+
+const constVariable = true
+console.log(typeof constVariable);
+console.log(constVariable);
