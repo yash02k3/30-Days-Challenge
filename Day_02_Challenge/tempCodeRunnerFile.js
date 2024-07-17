99208b71703c93e39d8b34a07d1a14f862ff80e4
Yash Kumar <dev.yash2k03@gmail.com>
2024-07-17 15:35:03 +0530

    add

---
+++ Day_02_Challenge/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+
+ function multiplynumbers(num5,num6){
+    let result = num5 * num6;
+    console.log("The result of the multiplication is: " + result);
+ }
+ let num5 = 102;
+ let num6 = 2;
+ multiplynumbers(num5, num6);
